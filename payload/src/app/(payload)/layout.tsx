936e79a1fde650e4766323cdeb17b21c7bb7e285
Vaidya Vrindavanam Dev <dev@vaidyavrindavanam.com>
2026-04-13 04:33:23 +0200

    fix: correct relative import path for payload.config in layout.tsx

--- payload/src/app/(payload)/layout.tsx
+++ payload/src/app/(payload)/layout.tsx
@@ -1,7 +1,7 @@
 import { RootLayout } from '@payloadcms/next/layouts'
 import React from 'react'
 import { importMap } from './admin/importMap.js'
-import config from '../../../payload.config'
+import config from '../../payload.config'
 
 type Args = {
   children: React.ReactNode
